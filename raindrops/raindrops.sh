e2c9c734bbba077e9a4714d95ae767385d2d94a4
Roman Ness <roman.ness@cosee.biz>
2019-05-22 20:44:23 +0200

    add stream-style solution for raindrops based on pipes

---
+++ raindrops/raindrops.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+function filter_factors {
+    while read n; do
+        if (( NUMBER % n == 0 )); then
+            echo $n
+        fi
+    done
+}
+
+function factors_to_raindrops {
+    while read n; do
+        case $n in
+            3) echo -n "Pling"
+            ;;
+            5) echo -n "Plang";
+            ;;
+            7) echo -n "Plong";
+            ;;
+        esac
+    done
+}
+
+function use_largest_factor_if_no_raindrops {
+    read raindrops || true
+    echo -n "${raindrops:-$NUMBER}"
+}
+
+function main {
+
+    # accept only a single integer
+    if (( $# != 1 )) || [[ ! "$1" =~ ^[1-9][0-9]*$ ]]; then
+        echo "Usage: $0 <integer>"; exit 1;
+    fi
+
+    NUMBER=$1
+    seq 1 $NUMBER | filter_factors | factors_to_raindrops | use_largest_factor_if_no_raindrops
+    exit 0;
+}
+
+main "$@"
