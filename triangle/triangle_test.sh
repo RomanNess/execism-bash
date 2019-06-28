c2c6b7fc14f6227fe8fb2afcba205182ff3b3369
Roman Ness <roman.ness@cosee.biz>
2019-06-28 16:32:11 +0200

    add additional tests for float-handling and error-handling in 'triangle'

--- triangle/triangle_test.sh
+++ triangle/triangle_test.sh
@@ -30,11 +30,25 @@
   [ "$output" == "false" ]
 }
 
+@test "All zero sides are illegal, so the triangle is not equilateral (with floats)" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash triangle.sh equilateral 0.0 0.00 0.000
+  [ "$status" -eq 0 ]
+  [ "$output" == "false" ]
+}
+
 # Bonus: deal with floats
 
 @test "sides may be floats" {
   #[[ $BATS_RUN_SKIPPED == true  ]] || skip
   run bash triangle.sh equilateral 0.5 0.5 0.5
+  [ "$status" -eq 0 ]
+  [ "$output" == "true" ]
+}
+
+@test "sides may be floats with different precision" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash triangle.sh equilateral 0.5 0.50 0.500
   [ "$status" -eq 0 ]
   [ "$output" == "true" ]
 }
@@ -83,11 +97,25 @@
   [ "$output" == "false" ]
 }
 
+@test "Sides that violate triangle inequality are not isosceles, even if two are equal (with floats)" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash triangle.sh isosceles 1 1.0 3.00
+  [ "$status" -eq 0 ]
+  [ "$output" == "false" ]
+}
+
 # Bonus: deal with floats
 
 @test "sides may be floats" {
   #[[ $BATS_RUN_SKIPPED == true  ]] || skip
   run bash triangle.sh isosceles 0.5 0.4 0.5
+  [ "$status" -eq 0 ]
+  [ "$output" == "true" ]
+}
+
+@test "sides may be floats with different precision" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash triangle.sh isosceles 0.5 0.40 0.500
   [ "$status" -eq 0 ]
   [ "$output" == "true" ]
 }
@@ -122,6 +150,13 @@
   [ "$output" == "false" ]
 }
 
+@test "Sides that violate triangle inequality are not scalene, even if they are all different (with floats)" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash triangle.sh scalene 7 3.0 2.00
+  [ "$status" -eq 0 ]
+  [ "$output" == "false" ]
+}
+
 # Bonus: deal with floats
 
 @test "sides may be floats" {
@@ -131,3 +166,27 @@
   [ "$output" == "true" ]
 }
 
+@test "sides may be floats with different precision" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash triangle.sh scalene 0.5 0.40 0.600
+  [ "$status" -eq 0 ]
+  [ "$output" == "true" ]
+}
+
+@test "print usage on wrong mode" {
+  run bash triangle.sh unknwon 1 1 1
+  [ "$status" -eq 1 ]
+  [ "$output" = "Usage: triangle.sh <scalene|equilateral|isosceles> <triangle-side> <triangle-side> <triangle-side>" ]
+}
+
+@test "print usage on to few params" {
+  run bash triangle.sh scalene 1 1
+  [ "$status" -eq 1 ]
+  [ "$output" = "Usage: triangle.sh <scalene|equilateral|isosceles> <triangle-side> <triangle-side> <triangle-side>" ]
+}
+
+@test "print usage on to many params" {
+  run bash triangle.sh scalene 1 1 1 1
+  [ "$status" -eq 1 ]
+  [ "$output" = "Usage: triangle.sh <scalene|equilateral|isosceles> <triangle-side> <triangle-side> <triangle-side>" ]
+}
