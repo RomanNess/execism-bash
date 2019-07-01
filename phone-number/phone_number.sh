aeb9d8b83595e93ab9bc49e15038db3a8ac53dd3
Roman Ness <roman.ness@cosee.biz>
2019-07-01 18:53:18 +0200

    find more elegant solution to strip optional country code in 'phone-number'

--- phone-number/phone_number.sh
+++ phone-number/phone_number.sh
@@ -3,28 +3,17 @@
 set -o errexit
 set -o nounset
 
-function reportInvalidNumber {
-    echo "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9"
-    exit 1
-}
-
-function trimCountryCodeIfPresent {
-    local numericPhoneNumber="$1"
-    if (( ${#numericPhoneNumber} == 11 )) && [[ "${numericPhoneNumber:0:1}" = "1" ]]; then
-        phoneNumber="${numericPhoneNumber:1}"
-    fi
-}
-
 function main {
     if (( $# != 1 )); then
         echo "Usage: $0 <phone-number>"
     fi
 
     local phoneNumber="${1//[!0-9]/}"
-    trimCountryCodeIfPresent "${phoneNumber}"
+    phoneNumber="${phoneNumber#1}"
 
     if [[ ! "${phoneNumber}" =~ ^([2-9][0-9]{2}){2}[0-9]{4}$ ]]; then
-        reportInvalidNumber
+        echo "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9"
+        exit 1
     fi
 
     echo "${phoneNumber}"
